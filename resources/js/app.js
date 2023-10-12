f0a39bcfdba77d8471ed41917c467a29ea2866cb
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2023-10-11 23:35:25 -0600

    feat(js): se implementa el script para power grid

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,6 @@
 import './bootstrap';
 import * as bootstrap from 'bootstrap'
+import './../../vendor/power-components/livewire-powergrid/dist/powergrid.js'
+import '../../vendor/power-components/livewire-powergrid/dist/bootstrap5.css'
+
+window.bootstrap = bootstrap
