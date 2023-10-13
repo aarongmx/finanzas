05c6943a10caa032bbf7276773319ddae5a3d338
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2023-10-13 00:59:39 -0600

    feat(input): se crea componente de input

---
+++ resources/views/components/form/input.blade.php
@@ -0,0 +1,23 @@
+@props([
+    'id' => Str::random(),
+    'type' => 'text',
+    'label' => 'Label'
+])
+
+<div class="mb-3">
+    <label for="{{$id}}" class="form-label">{{$label}}</label>
+    <input
+        id="{{$id}}"
+        type="{{$type}}"
+        @class([
+            'form-control',
+            'is-invalid' => $errors->has($attributes->wire('model')->value()),
+        ])
+        {{$attributes}}
+    >
+    @error($attributes->wire('model')->value())
+    <div class="invalid-feedback">
+        {{$message}}
+    </div>
+    @enderror
+</div>
