c52eb49ac4905f9ffd3118e30f8871671442e0ce
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2023-10-13 02:20:00 -0600

    fix(factory): se corrige factory de item cuenta

--- database/factories/ItemCuentaFactory.php
+++ database/factories/ItemCuentaFactory.php
@@ -19,14 +19,14 @@ class ItemCuentaFactory extends Factory
     public function definition(): array
     {
         return [
-            'pecio' => fake()->randomFloat(2, 1),
+            'precio' => fake()->randomFloat(2, 1),
             'cantidad_existencia' => fake()->randomFloat(2, 1),
             'importe_existencia' => fake()->randomFloat(2, 1), 'cantidad_entrada' => fake()->randomFloat(2, 1),
             'importe_entrada' => fake()->randomFloat(2, 1), 'cantidad_salida' => fake()->randomFloat(2, 1),
             'importe_salida' => fake()->randomFloat(2, 1), 'cantidad_sobrante' => fake()->randomFloat(2, 1),
             'importe_sobrante' => fake()->randomFloat(2, 1),
             'producto_id' => Producto::factory()->create()->id,
-            'cuenta' => Cuenta::factory()->create()->id,
+            'cuenta_id' => Cuenta::factory()->create()->id,
         ];
     }
 }
