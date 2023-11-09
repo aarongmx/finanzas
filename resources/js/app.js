455bcd309e09fd68938e039e78932df22383dd22
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2023-11-09 01:31:39 -0600

    feat(close-moda/swal): se crean eventos de js para cerrar modales y notificar al usuario

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,5 +2,18 @@ import './bootstrap';
 import * as bootstrap from 'bootstrap'
 import './../../vendor/power-components/livewire-powergrid/dist/powergrid.js'
 import '../../vendor/power-components/livewire-powergrid/dist/bootstrap5.css'
+import Swal from "sweetalert2";
 
 window.bootstrap = bootstrap
+
+document.addEventListener('livewire:initialized', () => {
+    Livewire.on('closeModal', (event) => {
+        const {modal} = event
+        bootstrap.Modal.getInstance(`#${modal}`).hide()
+    });
+
+    Livewire.on('notify', (event) => {
+        const {title, text, icon = 'success'} = event
+        Swal.fire(title, text, icon)
+    });
+});
