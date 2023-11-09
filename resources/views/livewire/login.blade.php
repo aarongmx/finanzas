7924142774dcced3f25c5fef2efaac37932363a4
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2023-11-09 16:55:38 -0600

    feat(login): se mejora login

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -1,25 +1,24 @@
-<div class="container">
-    <div class="row">
-        <div class="col-md-5">
-            <img src="{{asset('img/logo.svg')}}" alt="Logo Oficial ML Grupo Comercial">
-            <form wire:submit.prevent="login">
-                <x-form.input
-                    id="correo"
-                    type="email"
-                    label="Correo"
-                    wire:model="form.email"
-                />
-                <x-form.input
-                    id="password"
-                    type="password"
-                    label="Contraseña"
-                    wire:model="form.password"
-                />
-                <button type="submit" class="btn btn-primary">Iniciar sesión</button>
-            </form>
-        </div>
-        <div class="col-md-7">
-
+<div class="grid" style="--bs-gap: 1rem;">
+    <div class="g-col-12 g-col-md-4 g-start-md-5">
+        <div class="card">
+            <div class="card-body">
+                <img src="{{asset('img/logo.svg')}}" alt="Logo Oficial ML Grupo Comercial" width="186">
+                <form wire:submit.prevent="login">
+                    <x-form.input
+                        id="correo"
+                        type="email"
+                        label="Correo"
+                        wire:model="form.email"
+                    />
+                    <x-form.input
+                        id="password"
+                        type="password"
+                        label="Contraseña"
+                        wire:model="form.password"
+                    />
+                    <button type="submit" class="btn btn-primary btn-block">Iniciar sesión</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
