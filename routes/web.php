8742600a2a6b375d29f314c1878be1019a90c4da
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2023-11-08 20:36:38 -0600

    feat(routes): se agregan rutas

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::name('clientes.')->prefix('clientes')->group(function(){
+Route::get('/login', \App\Livewire\Login::class);
+
+Route::name('clientes.')->prefix('clientes')->group(function () {
     Route::get('/', \App\Livewire\Clientes\Index::class)->name('index');
+    Route::get('/nuevo', \App\Livewire\Clientes\Form::class)->name('form');
 });
 
 Route::name('sucursales.')->prefix('sucursales')->group(function () {
     Route::get('/', \App\Livewire\Sucursales\Index::class)->name('index');
+});
+
+Route::name('cuentas.')->prefix('cuentas')->group(function () {
+    Route::get('/', \App\Livewire\Cuentas\Index::class)->name('index');
 });
 
 
