fba1cf218a7308614153d7b0e186e661f5ffaf70
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2023-11-08 21:14:40 -0600

    chore(clientes): se remplaza el código de la modal por el componente

--- resources/views/livewire/clientes/index.blade.php
+++ resources/views/livewire/clientes/index.blade.php
@@ -1,4 +1,7 @@
 <div class="container py-4">
+    <x-modal id="exampleModal">
+        <livewire:clientes.form/>
+    </x-modal>
     <div class="row mb-3">
         <div class="col-12">
             <h1>Clientes</h1>
@@ -10,26 +13,6 @@
             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 Launch demo modal
             </button>
-
-            <!-- Modal -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-                 aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <livewire:clientes.form/>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
     <div class="row">
