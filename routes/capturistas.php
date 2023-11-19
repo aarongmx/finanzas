9717d949031551839a1b423bc097b25dfaa4c5cd
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2023-11-19 02:28:20 -0600

    feat(home): se agrega home del capturista

--- routes/capturistas.php
+++ routes/capturistas.php
@@ -1,5 +1,7 @@
 <?php
 
+Route::get('/home', \App\Livewire\Capturista\Home\Index::class)->name('home');
+
 Route::name('cuenta.')->prefix('cuenta')->group(function () {
     Route::get('/nuevas', \App\Livewire\Cuentas\Sucursal\Form::class)->name('sucursal');
 });
