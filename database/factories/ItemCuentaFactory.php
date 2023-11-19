a2a845554e9ce2cec0948de481e48d6e328e9c95
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2023-11-19 05:32:13 -0600

    fix(seeder): se cambia el procesamiento de items de cuenta seeder

--- database/factories/ItemCuentaFactory.php
+++ database/factories/ItemCuentaFactory.php
@@ -18,13 +18,22 @@ class ItemCuentaFactory extends Factory
      */
     public function definition(): array
     {
+        $precio = fake()->randomFloat(2, 1);
+        $cantidadExistencia = fake()->randomFloat(2, 1);
+        $cantidadEntrada = fake()->randomFloat(2, 1);
+        $cantidadSalida = fake()->randomFloat(2, 1);
+        $cantidadSobrante = fake()->randomFloat(2, 1);
+
         return [
-            'precio' => fake()->randomFloat(2, 1),
-            'cantidad_existencia' => fake()->randomFloat(2, 1),
-            'importe_existencia' => fake()->randomFloat(2, 1), 'cantidad_entrada' => fake()->randomFloat(2, 1),
-            'importe_entrada' => fake()->randomFloat(2, 1), 'cantidad_salida' => fake()->randomFloat(2, 1),
-            'importe_salida' => fake()->randomFloat(2, 1), 'cantidad_sobrante' => fake()->randomFloat(2, 1),
-            'importe_sobrante' => fake()->randomFloat(2, 1),
+            'precio' => $precio,
+            'cantidad_existencia' => $cantidadExistencia,
+            'importe_existencia' => $precio * $cantidadExistencia,
+            'cantidad_entrada' => $cantidadEntrada,
+            'importe_entrada' => $precio * $cantidadExistencia,
+            'cantidad_salida' => $cantidadSalida,
+            'importe_salida' => $precio * $cantidadSalida,
+            'cantidad_sobrante' => $cantidadSobrante,
+            'importe_sobrante' => $precio * $cantidadSobrante,
             'producto_id' => Producto::factory()->create()->id,
             'cuenta_id' => Cuenta::factory()->create()->id,
         ];
