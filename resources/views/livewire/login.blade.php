49a268ac3e86a3ffa23a48f90e9a365dfa7e5cdb
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2023-11-19 03:59:54 -0600

    fix(style): se corrige estilo del login

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -1,8 +1,10 @@
 <div class="grid min-vh-100" style="--bs-gap: 1rem; background: url({{asset('img/background.jpg')}}) no-repeat center / cover;">
     <div class="g-col-12 g-col-md-4 g-start-md-5">
         <div class="card">
-            <div class="card-body">
+            <div class="card-header border-0 text-center">
                 <img src="{{asset('img/logo.svg')}}" alt="Logo Oficial ML Grupo Comercial" width="186">
+            </div>
+            <div class="card-body">
                 <form wire:submit.prevent="login">
                     <x-form.input
                         id="correo"
