ac1d8c06349cfa5bc5a0d8ea1986a283a82c4587
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2023-12-02 06:29:01 -0600

    feat(sucursales): se agregan las sucursales

--- database/seeders/SucursalesSeeder.php
+++ database/seeders/SucursalesSeeder.php
@@ -16,10 +16,33 @@ class SucursalesSeeder extends Seeder
     {
         $dir = Direccion::factory()->create();
         collect([
+            ['nombre' => 'COPILCO', 'direccion_id' => $dir->id],
             ['nombre' => 'TOTOI', 'direccion_id' => $dir->id],
-            ['nombre' => 'AZTECAS', 'direccion_id' => $dir->id],
+            ['nombre' => 'ESCUINAPA', 'direccion_id' => $dir->id],
+            ['nombre' => 'TEPETLAPA', 'direccion_id' => $dir->id],
+            ['nombre' => 'OPICHEN', 'direccion_id' => $dir->id],
+            ['nombre' => 'TETIZ', 'direccion_id' => $dir->id],
+            ['nombre' => 'GRUPO DZ', 'direccion_id' => $dir->id],
+            ['nombre' => 'GRUPO COMERCIAL', 'direccion_id' => $dir->id],
+            ['nombre' => 'SRA CASTILLO', 'direccion_id' => $dir->id],
+            ['nombre' => 'LUZ', 'direccion_id' => $dir->id],
+            ['nombre' => 'RAMÓN', 'direccion_id' => $dir->id],
+            ['nombre' => 'LAS ROSAS', 'direccion_id' => $dir->id],
+            ['nombre' => 'CARNE', 'direccion_id' => $dir->id],
+            ['nombre' => 'EL CRISTO - FAMSA', 'direccion_id' => $dir->id],
+            ['nombre' => 'MH', 'direccion_id' => $dir->id],
+            ['nombre' => 'BODEGA', 'direccion_id' => $dir->id],
+            ['nombre' => 'SEÑOR LADRON', 'direccion_id' => $dir->id],
+            ['nombre' => 'TEPETLAPA ML', 'direccion_id' => $dir->id],
+            ['nombre' => 'PEESCDITOS 2', 'direccion_id' => $dir->id],
+            ['nombre' => 'PESCADITOS', 'direccion_id' => $dir->id],
+            ['nombre' => 'LA CANDE', 'direccion_id' => $dir->id],
+            ['nombre' => 'SOLUCION ALIMENTARIA', 'direccion_id' => $dir->id],
+            ['nombre' => 'ROSAS 1', 'direccion_id' => $dir->id],
+            ['nombre' => 'TLALCOLIGIA', 'direccion_id' => $dir->id],
+            ['nombre' => 'BODEGUITA', 'direccion_id' => $dir->id],
         ])->each(function ($sucursal) {
-           Sucursal::create($sucursal);
+            Sucursal::create($sucursal);
         });
     }
 }
