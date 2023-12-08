3d8b433f5fcbfb43ed1e7a009840f9de802795ba
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2023-12-07 22:36:13 -0600

    fix(button): se elimina botón de test

--- resources/views/livewire/administrador/cuentas/form.blade.php
+++ resources/views/livewire/administrador/cuentas/form.blade.php
@@ -2,7 +2,6 @@
     <div class="row">
         <div class="col-12">
             <h1>Cuenta</h1>
-            <button wire:click.prevent="tests">test</button>
         </div>
     </div>
     <div class="row">
