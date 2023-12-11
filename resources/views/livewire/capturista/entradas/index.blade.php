6be854860270579d20970492653b951661ae8c88
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2023-12-11 02:36:13 -0600

    fix(index)

--- resources/views/livewire/capturista/entradas/index.blade.php
+++ resources/views/livewire/capturista/entradas/index.blade.php
@@ -1,4 +1,9 @@
-<div class="container">
+<div class="container-fluid p-4">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="h3">Entradas</h1>
+        </div>
+    </div>
     <div class="row">
         <div class="col-12">
             <livewire:capturista.entrada-table/>
