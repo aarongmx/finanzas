0db4fb5ca60bcfc472e9ef92df54b5de1be34fcc
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2024-01-04 01:09:27 -0600

    fix(productos): se mejora el seeder de producto

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
             CategoriasSeeder::class,
             PermissionsSeeder::class,
             UsuariosSeeder::class,
-            #ProductosSeeder::class,
+            ProductosSeeder::class,
             EstatusSeeder::class,
         ]);
     }
