fac57561d9b70c6bcf5eba3db95db9634cfb5423
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2024-01-06 05:02:50 -0600

    fix(table): se quita padding y se agregan attributes

--- resources/views/components/table/table.blade.php
+++ resources/views/components/table/table.blade.php
@@ -3,8 +3,8 @@
     'min' => 60,
 ])
 <div class="card overflow-hidden">
-    <div class="card-body table-responsive" style="max-height: 60vh; min-height: {{$min}}vh;">
-        <table class="table table-hover table-striped">
+    <div class="card-body p-0 table-responsive" style="max-height: 60vh; min-height: {{$min}}vh;">
+        <table class="table table-hover table-striped" {{$attributes}}>
             @if($header)
                 <thead>
                 {{$header}}
