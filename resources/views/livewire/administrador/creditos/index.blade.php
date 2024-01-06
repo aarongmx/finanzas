8439f9f2960e470997b610dad4da44de954c6bf2
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2024-01-06 15:21:10 -0600

    feat(index): se agrega titulo a la página

--- resources/views/livewire/administrador/creditos/index.blade.php
+++ resources/views/livewire/administrador/creditos/index.blade.php
@@ -1,4 +1,9 @@
-<div class="container-fluid">
+<div class="container-fluid py-4">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="h2">Créditos</h1>
+        </div>
+    </div>
     <div class="row">
         <div class="col-12">
             <livewire:credito-table/>
