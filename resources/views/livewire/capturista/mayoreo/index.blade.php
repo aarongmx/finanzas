694c2b33e644cae80853b45fe96728ad994870e8
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2024-01-06 16:41:17 -0600

    fix(view)

--- resources/views/livewire/capturista/mayoreo/index.blade.php
+++ resources/views/livewire/capturista/mayoreo/index.blade.php
@@ -1,10 +1,10 @@
-<div class="container py-lg-4">
+<div class="container-fluid py-lg-4">
     <x-modal id="mayoreo">
         <livewire:capturista.mayoreo.form/>
     </x-modal>
     <div class="row">
         <div class="col-12 d-flex align-items-center justify-content-between">
-            <h1>Mayoreos</h1>
+            <h1 class="h3">Mayoreos</h1>
             <x-button data-bs-target="#mayoreo" data-bs-toggle="modal">Nuevo</x-button>
         </div>
     </div>
