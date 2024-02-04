b7c64f7c3ba723d39e15492062a685f7240b7a31
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2024-02-04 03:32:33 -0600

    feat(form): se agrega formulario para registrar cuenta

--- app/Livewire/Administrador/Cuentas/Form.php
+++ app/Livewire/Administrador/Cuentas/Form.php
@@ -7,12 +7,19 @@ use App\Models\Categoria;
 use App\Models\ItemCuenta;
 use App\Models\Producto;
 use App\Models\Sucursal;
+use Carbon\Carbon;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Form extends Component
 {
     #public CuentaForm $form;
+
+    public $fechaVenta;
+
+    public $fechaCaptura;
+
+    public $sucursal;
 
     public $items = [];
 
@@ -32,24 +39,31 @@ class Form extends Component
 
     public function mount()
     {
-        $this->items = Producto::query()
-            ->with([
-                'itemsCuenta' => fn($q) => $q->whereHas('cuenta', fn($q) => $q->where('fecha_venta', today()->subDay()))->limit(1)
-            ])
-            ->get()
-            ->map(function ($producto) {
-                $item = $producto?->itemsCuenta?->first();
-                return [
-                    'producto_name' => $producto->nombre,
-                    'producto_id' => $producto->id,
-                    'precio' => $item->precio ?? 0,
-                    'importe_existencia' => $item->importe_existencia ?? 0,
-                    'cantidad_existencia' => $item->cantidad_existencia ?? 0,
-                    'cantidad_sobrante' => $producto->categoria_id === 2 ? ($item?->cantidad_sobrante) ?? 0 : 0,
-                    'importe_sobrante' => $producto->categoria_id === 2 ? ($item?->importe_sobrante) ?? 0 : 0,
-                    'categoria_id' => $producto->categoria_id,
-                ];
-            })->toArray();
+        $this->fechaCaptura = today()->format('Y-m-d');
+    }
+
+    public function updatedFechaVenta($value)
+    {
+        if ($value) {
+            $this->items = Producto::query()
+                ->with([
+                    'itemsCuenta' => fn($q) => $q->whereHas('cuenta', fn($q) => $q->where('fecha_venta', Carbon::parse($this->fechaVenta)->subDay()))->limit(1)
+                ])
+                ->get()
+                ->map(function ($producto) {
+                    $item = $producto?->itemsCuenta?->first();
+                    return [
+                        'producto_name' => $producto->nombre,
+                        'producto_id' => $producto->id,
+                        'precio' => $item->precio ?? 0,
+                        'importe_existencia' => $item->importe_existencia ?? 0,
+                        'cantidad_existencia' => $item->cantidad_existencia ?? 0,
+                        'cantidad_sobrante' => $producto->categoria_id === 2 ? ($item?->cantidad_sobrante) ?? 0 : 0,
+                        'importe_sobrante' => $producto->categoria_id === 2 ? ($item?->importe_sobrante) ?? 0 : 0,
+                        'categoria_id' => $producto->categoria_id,
+                    ];
+                })->toArray();
+        }
     }
 
     public function render()
--- resources/views/livewire/administrador/cuentas/form.blade.php
+++ resources/views/livewire/administrador/cuentas/form.blade.php
@@ -6,136 +6,132 @@
     </div>
     <div class="row">
         <div class="col-12 col-md-4">
-            <select class="form-select">
-                @forelse($this->sucursales as $sucursal)
-                    <option value="{{$sucursal->id}}" wire:key="{{$sucursal->id}}">{{$sucursal->nombre}}</option>
-                @empty
-                @endforelse
-            </select>
+            <div class="form-floating">
+                <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+                    <option selected>-- Seleccione una sucursal --</option>
+                    @forelse($this->sucursales as $sucursal)
+                        <option value="{{$sucursal->id}}" wire:key="{{$sucursal->id}}">{{$sucursal->nombre}}</option>
+                    @empty
+                    @endforelse
+                </select>
+                <label for="floatingSelect">Sucursales</label>
+            </div>
         </div>
         <div class="col-12 col-md-4">
             <x-form.input
                 label="Fecha de venta"
                 type="date"
+                wire:model.live.debounce.350ms="fechaVenta"
             />
         </div>
         <div class="col-12 col-md-4">
             <x-form.input
                 label="Fecha de captura"
                 type="date"
+                wire:model="fechaCaptura"
             />
         </div>
     </div>
     <div class="row">
         <div class="col-12">
-            @forelse($this->categorias as $categoria)
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="{{$categoria->nombre}}"
-                           id="categoria-{{$categoria->id}}" value="{{$categoria->id}}"
-                           wire:model.live="categoriaSeleccionada">
-                    <label class="form-check-label" for="categoria-{{$categoria->id}}">{{$categoria->nombre}}</label>
-                </div>
-            @empty
-            @endforelse
-        </div>
-    </div>
-
-    <x-table.table>
-        <x-slot:header>
-            <x-table.th>Producto</x-table.th>
-            <x-table.th>Precio</x-table.th>
-            <x-table.th>Sobrante anterior(KG)</x-table.th>
-            <x-table.th>Importe anterior($)</x-table.th>
-            <x-table.th>Entrada(KG)</x-table.th>
-            <x-table.th>Importe Entrada($)</x-table.th>
-            <x-table.th>Salida(KG)</x-table.th>
-            <x-table.th>Importe Salida($)</x-table.th>
-            <x-table.th>Sobrante(KG)</x-table.th>
-            <x-table.th>Importe Sobrante($)</x-table.th>
-        </x-slot:header>
-        @forelse($this->items as $i => $item)
-            <tr wire:key="{{$i}}">
-                <td>
-                    <p>{{$item['producto_name']}}</p>
-                </td>
-                <td>
-                    <x-form.input-table
-                        wire:model="items.{{$i}}.precio"
-                        @input="(e) => {
-                            let quantity = $wire.items[{{$i}}].cantidad_existencia;
-                            let value = e.target.value;
-                            $wire.items[{{$i}}].importe_existencia = quantity * value;
+            <x-table.table>
+                <x-slot:header>
+                    <x-table.th>Producto</x-table.th>
+                    <x-table.th>Precio</x-table.th>
+                    <x-table.th>Sobrante anterior(KG)</x-table.th>
+                    <x-table.th>Importe anterior($)</x-table.th>
+                    <x-table.th>Entrada(KG)</x-table.th>
+                    <x-table.th>Importe Entrada($)</x-table.th>
+                    <x-table.th>Salida(KG)</x-table.th>
+                    <x-table.th>Importe Salida($)</x-table.th>
+                    <x-table.th>Sobrante(KG)</x-table.th>
+                    <x-table.th>Importe Sobrante($)</x-table.th>
+                </x-slot:header>
+                @forelse($this->items as $i => $item)
+                    <tr wire:key="{{$i}}">
+                        <td>
+                            <p>{{$item['producto_name']}}</p>
+                        </td>
+                        <td>
+                            <x-form.input-table
+                                wire:model="items.{{$i}}.precio"
+                                @input="(e) => {
+                            let quantity = parseFloat($wire.items[{{$i}}].cantidad_existencia).toFixed(2);
+                            let value = parseFloat(e.target.value).toFixed(2);
+                            $wire.items[{{$i}}].importe_existencia = parseFloat(quantity * value).toFixed(2);
                        }"
-                    />
-                </td>
-                <td>
-                    <x-form.input-table
-                        wire:model="items.{{$i}}.cantidad_existencia"
-                        disabled
-                    />
-                </td>
-                <td>
-                    <x-form.input-table
-                        wire:model="items.{{$i}}.importe_existencia"
-                        disabled
-                    />
-                </td>
-                <td>
-                    <x-form.input-table
-                        wire:model="items.{{$i}}.cantidad_entrada"
-                        @input="(e) => {
-                            let price = $wire.items[{{$i}}].precio;
-                            let value = e.target.value;
-                            $wire.items[{{$i}}].importe_entrada = price * value;
+                            />
+                        </td>
+                        <td>
+                            <x-form.input-table
+                                wire:model="items.{{$i}}.cantidad_existencia"
+                                disabled
+                            />
+                        </td>
+                        <td>
+                            <x-form.input-table
+                                wire:model="items.{{$i}}.importe_existencia"
+                                disabled
+                            />
+                        </td>
+                        <td>
+                            <x-form.input-table
+                                wire:model="items.{{$i}}.cantidad_entrada"
+                                @input="(e) => {
+                            let price = parseFloat($wire.items[{{$i}}].precio).toFixed(2);
+                            let value = parseFloat(e.target.value).toFixed(2);
+                            $wire.items[{{$i}}].importe_entrada = parseFloat(price * value).toFixed(2);
                        }"
-                    />
-                </td>
-                <td>
-                    <x-form.input-table
-                        wire:model="items.{{$i}}.importe_entrada"
-                        disabled
-                    />
-                </td>
-                <td>
-                    <x-form.input-table
-                        wire:model="items.{{$i}}.cantidad_salida"
-                        @input="(e) => {
-                            let price = $wire.items[{{$i}}].precio;
-                            let value = e.target.value;
-                            $wire.items[{{$i}}].importe_salida = price * value;
+                            />
+                        </td>
+                        <td>
+                            <x-form.input-table
+                                wire:model="items.{{$i}}.importe_entrada"
+                                disabled
+                            />
+                        </td>
+                        <td>
+                            <x-form.input-table
+                                wire:model="items.{{$i}}.cantidad_salida"
+                                @input="(e) => {
+                            let price = parseFloat($wire.items[{{$i}}].precio).toFixed(2);
+                            let value = parseFloat(e.target.value).toFixed(2);
+                            $wire.items[{{$i}}].importe_salida = parseFloat(price * value).toFixed(2);
 
                             if($wire.items[{{$i}}].categoria_id === 2){
-                                let amount = {{$item['cantidad_sobrante']}} - value;
-                                $wire.items[{{$i}}].cantidad_sobrante = amount;
-                                $wire.items[{{$i}}].importe_sobrante = price * amount;
+                                let amount = parseFloat({{$item['cantidad_sobrante']}} - value).toFixed(2);
+                                $wire.items[{{$i}}].cantidad_sobrante = parseFloat(amount).toFixed(2);
+                                $wire.items[{{$i}}].importe_sobrante = parseFloat(price * amount).toFixed(2);
                             }
                        }"
-                    />
-                </td>
-                <td>
-                    <x-form.input-table
-                        wire:model="items.{{$i}}.importe_salida"
-                        disabled
-                    />
-                </td>
-                <td>
-                    <x-form.input-table
-                        wire:model="items.{{$i}}.cantidad_sobrante"
-                        @input="(e) => {
+                            />
+                        </td>
+                        <td>
+                            <x-form.input-table
+                                wire:model="items.{{$i}}.importe_salida"
+                                disabled
+                            />
+                        </td>
+                        <td>
+                            <x-form.input-table
+                                wire:model="items.{{$i}}.cantidad_sobrante"
+                                @input="(e) => {
                             let price = $wire.items[{{$i}}].precio;
                             let value = e.target.value;
                             $wire.items[{{$i}}].importe_sobrante = price * value;
                        }"
-                    />
-                </td>
-                <td>
-                    <x-form.input-table
-                        wire:model="items.{{$i}}.importe_sobrante"
-                        disabled
-                    />
-                </td>
-            </tr>
-        @empty
-        @endforelse
-    </x-table.table>
+                            />
+                        </td>
+                        <td>
+                            <x-form.input-table
+                                wire:model="items.{{$i}}.importe_sobrante"
+                                disabled
+                            />
+                        </td>
+                    </tr>
+                @empty
+                @endforelse
+            </x-table.table>
+        </div>
+    </div>
 </div>
